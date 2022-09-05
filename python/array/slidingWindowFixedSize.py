6a8183be19ac20e1c0de1b3278a834f0fe007f28
mouse <mouse>
2022-09-05 16:44:45 -0700

    Fix bug

--- python/array/slidingWindowFixedSize.py
+++ python/array/slidingWindowFixedSize.py
@@ -4,7 +4,7 @@
 # O(n * k)
 def closeDuplicatesBruteForce(nums, k):
     for L in range(len(nums)):
-        for R in range(L, min(len(nums), L + k)):
+        for R in range(L + 1, min(len(nums), L + k)):
             if nums[L] == nums[R]:
                 return True
     return False
