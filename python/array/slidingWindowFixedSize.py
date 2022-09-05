9dad1b3717bea8c8c1ec4a2dba22147d779d68df
mouse <mouse>
2022-09-05 16:57:38 -0700

    Fix bug

--- python/array/slidingWindowFixedSize.py
+++ python/array/slidingWindowFixedSize.py
@@ -16,7 +16,7 @@ def closeDuplicates(nums, k):
     L = 0
 
     for R in range(len(nums)):
-        if R - L > k:
+        if R - L + 1 > k:
             window.remove(nums[L])
             L += 1
         if nums[R] in window:
