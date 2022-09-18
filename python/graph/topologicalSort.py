7e6a17003eac91c5aba2245a688b58ca65ea7fb7
mouse <mouse>
2022-09-17 17:39:00 -0700

    Topological sort python

--- python/graph/topologicalSort.py
+++ python/graph/topologicalSort.py
@@ -11,14 +11,19 @@ def topologicalSort(edges, n):
         adj[src].append(dst)
 
     topSort = []
+    visit = set()
+    for i in range(1, n + 1):
+        dfs(i, adj, visit, topSort)
+    topSort.reverse()
+    return topSort
 
 def dfs(src, adj, visit, topSort):
     if src in visit:
-        return
+        return True
     visit.add(src)
     
     for neighbor in adj[src]:
-        dfs(neighbor)
+        dfs(neighbor, adj, visit, topSort)
     topSort.append(src)
 
 edges = [[1,2],[1,3],[2,4],[3,4],[5,6],[6,4],[6,8],[7,8]]
