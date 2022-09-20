2156b0280ec54b68f96260a68474fe6613f3f347
ranveersingh2718 <ranveersingh10600@gmail.com>
2022-09-20 00:24:45 -0700

    fix Lcs.java char[] to int[]

--- java/dynamicProgramming/Lcs.java
+++ java/dynamicProgramming/Lcs.java
@@ -21,23 +21,23 @@ public class Lcs {
     // Time: O(n * m), Space: O(n + m)
     public static int memoization(String s1, String s2) {
         int N = s1.length(), M = s2.length();
-        char[][] cache = new char[N][M];
+        int[][] cache = new int[N][M];
         return memoHelper(s1, s2, 0, 0, cache);
     }
 
-    public static int memoHelper(String s1, String s2, int i1, int i2, char[][] cache) {
+    public static int memoHelper(String s1, String s2, int i1, int i2, int[][] cache) {
         if (i1 == s1.length() || i2 == s2.length()) {
             return 0;
         }
 
-        if (cache[i1][i2] != '\0') {
+        if (cache[i1][i2] != 0) {
             return cache[i1][i2];
         }
 
         if (s1.charAt(i1) == s2.charAt(i2)) {
-            cache[i1][i2] = (char) (1 + memoHelper(s1, s2, i1 + 1, i2 + 1, cache));
+            cache[i1][i2] = 1 + memoHelper(s1, s2, i1 + 1, i2 + 1, cache);
         } else {
-            cache[i1][i2] = (char) Math.max(memoHelper(s1, s2, i1 + 1, i2, cache), 
+            cache[i1][i2] = Math.max(memoHelper(s1, s2, i1 + 1, i2, cache), 
                                     memoHelper(s1, s2, i1, i2 + 1, cache));
         }
         return cache[i1][i2];
@@ -46,14 +46,14 @@ public class Lcs {
     // Time: O(n * m), Space: O(n + m)
     public static int dp(String s1, String s2) {
         int N = s1.length(), M = s2.length();
-        char[][] dp = new char[N+1][M+1];
+        int[][] dp = new int[N+1][M+1];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i+1][j+1] = (char) (1 + dp[i][j]);
+                    dp[i+1][j+1] = 1 + dp[i][j];
                 } else {
-                    dp[i+1][j+1] = (char) Math.max(dp[i][j+1], dp[i+1][j]);
+                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
                 }
             }
         }
@@ -63,15 +63,15 @@ public class Lcs {
     // Time: O(n * m), Space: O(m)
     public static int optimizedDp(String s1, String s2) {
         int N = s1.length(), M = s2.length();
-        char[] dp = new char[M + 1];
+        int[] dp = new int[M + 1];
 
         for (int i = 0; i < N; i++) {
-            char[] curRow = new char[M + 1];
+            int[] curRow = new int[M + 1];
             for (int j = 0; j < M; j++) {
                 if (s1.charAt(i) == s2.charAt(j)) {
-                    curRow[j+1] = (char) (1 + dp[j]);
+                    curRow[j+1] = 1 + dp[j];
                 } else {
-                    curRow[j+1] = (char) Math.max(dp[j + 1], curRow[j]);
+                    curRow[j+1] = Math.max(dp[j + 1], curRow[j]);
                 }
             }
             dp = curRow;
