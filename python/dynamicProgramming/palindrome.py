7d77adf19d9a1e4b147395343e20ca2c6defed0a
mouse <mouse>
2022-09-23 13:21:23 -0700

    Update palindrome

--- python/dynamicProgramming/palindrome.py
+++ python/dynamicProgramming/palindrome.py
@@ -21,3 +21,24 @@ def longest(s):
             
     return length
 
+
+# Same solution, without duplicate code.
+# Time: O(n^2), Space: O(n)
+def longest(s):
+    length = 0
+    for i in range(len(s)):
+        # odd length
+        length = max(length, helper(s, i, i))
+        
+        # even length
+        length = max(length, helper(s, i, i + 1)) 
+    return length
+
+def helper(s, l, r):
+    maxLength = 0
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        if (r - l + 1) > maxLength:
+            maxLength = r - l + 1
+        l -= 1
+        r += 1
+    return maxLength
